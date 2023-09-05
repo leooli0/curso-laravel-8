861fa16f81560f1516e3d84a5e11a299f3cfe81c
leo <leonardosantos300305@gmail.com>
2023-09-05 14:58:22 -0300

    visualizando posts e deletando

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,4 +26,28 @@ class PostController extends Controller
 
         return redirect()->route('posts.index');
     }
+
+    public function show($id)
+    {
+
+        //$post = Post::where('id', $id)->first();
+
+        if (!$post = Post::find($id)) {
+            return redirect()->route('posts.index');
+        }
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id)
+    {
+        if (!$post = Post::find($id))
+            return redirect()->route('posts.index');
+
+        $post->delete();
+
+        return redirect()
+            ->route('posts.index')
+            ->with('message', 'Post Deletado com Sucesso');
+    }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,15 @@
 <h1>Listanto Posts</h1>
 
+@if (session('message'))
+    <div>
+        {{ session('message') }}
+    </div>
+@endif
+
 @foreach ($posts as $post)
-    <p>{{ $post->title }}</p>
+    <p>
+        {{ $post->title }} [ <a href="{{ route('posts.show', $post->id) }}">Ver</a> ]
+    </p>
 @endforeach
 
 <hr>

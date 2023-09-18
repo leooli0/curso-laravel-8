62a80f10ea59004dfc1091005203d8cc82549de4
leo <leonardosantos300305@gmail.com>
2023-09-18 14:09:35 -0300

    Upload de Arquivos no Laravel pt 2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUpdatePost;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class PostController extends Controller
@@ -78,7 +79,21 @@ class PostController extends Controller
             return redirect()->back();
         }
 
-        $post->update($request->all());
+        $data = $request->all();
+
+        if ($request->image->isValid()) {
+            if (Storage::exists($post->image)) {
+                Storage::delete($post->image);
+            }
+
+            $nameFile = Str::of($request->title)->slug('-') . '.' . $request->image->getClientOriginalExtension();
+
+            $image = $request->image->storeAs('posts', $nameFile);
+            $data['image'] = $image;
+        }
+
+        $post->update($data);
+
 
         return redirect()
             ->route('posts.index')
@@ -89,6 +104,10 @@ class PostController extends Controller
     {
         if (!$post = Post::find($id))
             return redirect()->route('posts.index');
+
+        if (Storage::exists($post->image)) {
+            Storage::delete($post->image);
+        }
 
         $post->delete();
 
